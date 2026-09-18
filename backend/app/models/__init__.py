ee23d56b692501f5a02bf51ff08247684128f178
vinayakanw <vinayakanw@gmail.com>
2026-09-18 15:00:09 +0530

    update models init for transactions

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
 from app.models.user import user_document
 from app.models.business import business_document
 from app.models.invoice import invoice_document
+from app.models.transaction import transaction_document
