6cced0faf899809a2b5d09a8c16aee0b4398d10b
vinayakanw <vinayakanw@gmail.com>
2026-09-20 16:40:13 +0530

    build(deps): add speech_to_text, image_picker, flutter_tts, and mime dependencies

--- mobile/vypara_ai/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/vypara_ai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import file_selector_macos
 import flutter_secure_storage_macos
+import flutter_tts
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
